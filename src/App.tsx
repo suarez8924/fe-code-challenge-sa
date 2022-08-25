466a8519bf8e3fa0cbce6840e1ad739fe967ba27
unknown <suarezjav89@gmail.com>
2022-08-24 21:50:02 -0300

    install and configure prettier, eslint and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.scss';
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <h1>Hello there</h1>
+    </main>
+  );
 }
 
 export default App;
