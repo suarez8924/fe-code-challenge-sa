78c377cdc97f9db53c5823467c34cc67e2850769
unknown <suarezjav89@gmail.com>
2022-08-28 14:39:24 -0300

    create RepositoryList component

---
+++ src/components/RepositoriesList/RepositoriesList.tsx
@@ -0,0 +1,29 @@
+import { Repository } from "../../data/types";
+import RepositoryDetails from "../RepositoryDetails/RepositoryDetails";
+import "./RepositoryList.scss";
+
+interface RepositoriesListProps {
+  repositories: Array<Repository> | [];
+}
+function RepositoriesList(props: RepositoriesListProps) {
+  const { repositories } = props;
+  return (
+    <>
+      {repositories.length > 0 ? (
+        <ul>
+          {repositories.map((repository) => (
+            <li key={repository.id} className="repository-item">
+              <RepositoryDetails repositoryContent={repository} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>
+          <h2>No elements found</h2>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default RepositoriesList;
