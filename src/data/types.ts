c75fc0fa249a7378002c5ea70c7c96735e6baf34
unknown <suarezjav89@gmail.com>
2022-08-28 14:38:38 -0300

    update types.ts

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,12 +1,8 @@
-export type Owner = {
-  login: string;
-  avatarUrl: string;
-};
-
 export type Repository = {
   id: number;
   language: string;
   name: string;
-  url: string;
-  owner: Owner;
+  htmlUrl: string;
+  description: string;
+  stargazersCount: number;
 };
